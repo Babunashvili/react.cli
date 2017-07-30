3ed2af312bfaf528d0d7ddc680749fa3123b66ec
David Babunashvili <mac@Macs-MacBook-Pro.local>
2017-07-30 17:16:42 +0400

    remove extra files

--- src/app/components/Foo/Foo.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react'
-
-const Foo = props => (
-   <div>Component</div>
-);
-
-Foo.propTypes = {}
-
-export default Foo
--- src/app/components/Test/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react'
-
-const Test = props => (
-   <div>Component</div>
-);
-
-Test.propTypes = {}
-
-export default Test
